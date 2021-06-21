9b294abd5ca2aa4dd7270e5eb4e27bc08582846e
Remigijus Kiminas <remdex@gmail.com>
2021-06-21 09:24:50 -0400

    Escape template

--- design/mattermosttheme/tpl/lhmattermost/searchchannel.tpl.php
+++ design/mattermosttheme/tpl/lhmattermost/searchchannel.tpl.php
@@ -1,4 +1,4 @@
-<div class="py-2">
+<div class="py-2" ng-non-bindable>
     <?php foreach ($channels as $channel)  : ?>
         <label><button type="button" onclick='setChannelNotifications(<?php echo json_encode($channel)?>)' class="btn btn-xs btn-info"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/mattermost','set')?></button> <?php echo htmlspecialchars($channel['display_name'])?></label>
     <?php endforeach; ?>
